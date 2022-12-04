019dedfdf7cdf001146fc9f49c777f7ecf022d51
ASHISK404 <116161048+ASHISK404@users.noreply.github.com>
2022-12-04 20:27:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const title= document.querySelector('.title');
+const prev= document.querySelector('.prev');
+const playPause= document.querySelector('.playPause');
+const next= document.querySelector('.next');
+const audio= document.querySelector('audio');
+
+// song list
+const songList =[
+    {
+        // 你的音乐的路径,下面两个xxx处同理
+        path:"music/PV.mp3",
+        // 你的音乐的名字，下面两个xxx处同理
+        songName:"PV",
+    },
+];
+
+let songPlaying = false;
+
+//play song
+ const playSong = ()=>{
+    songPlaying = true;
+    audio.play();
+    playPause.classList.add('active');
+    playPause.innerHTML = '<span class="material-icons">暂停</span>';
+}
+
+//pause song
+const pauseSong = () => {
+    songPlaying = false;
+    audio.pause();
+    playPause.classList.remove('active');
+    playPause.innerHTML = ' <span class="material-icons">播放</span>';
+
+}
+//play/pause song
+playPause.addEventListener("click",() => (songPlaying? pauseSong() : playSong()));
+
+//load song
+const loadSong = (songList) => {
+    title.textContent = songList.songName;
+    audio.src = songList.path;//
+
+}
+let i = 0;
+loadSong (songList[i]);
